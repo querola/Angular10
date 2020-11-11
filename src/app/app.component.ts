339cb0557aeb0d7e793e51336530338e16100373
querola <ramr16@outlook.com>
2020-11-11 11:29:56 -0600

    Todo bien hasta la clase 20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front-end';
+
+  pelicula = {
+    titulo: 'Spider-Man',
+    fechaLanzamiento: new Date(),
+    precio : 1499.99
+  };
+
+  duplicarNumero(valor: number): number {
+    return valor * 2;
+  };
 }
