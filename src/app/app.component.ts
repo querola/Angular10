585b195e240aa7cfe64132b5da6d9b2b45a6a9e4
querola <ramr16@outlook.com>
2020-11-12 10:07:19 -0600

    Todo bien hasta la clase 22

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.peliculas = [{
+        titulo: 'Spider-Man',
+        fechaLanzamiento: new Date(),
+        precio: 1499.99
+      },{
+        titulo: 'toy story',
+        fechaLanzamiento: new Date('2000-11-15'),
+        precio: 499.99
+      }]
+    }, 1000);
+  }
   title = 'front-end';
 
-  pelicula = {
-    titulo: 'Spider-Man',
-    fechaLanzamiento: new Date(),
-    precio : 1499.99
-  };
+  peliculas;
 
   duplicarNumero(valor: number): number {
     return valor * 2;
