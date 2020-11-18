7fd7fa7996133902f9cebcef50b30fdf8f5455b2
Rodrigo Mendoza <el_stroke_666@hotmail.com>
2020-11-17 20:26:58 -0600

    todo bien hasta la clase 29

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,25 +17,12 @@ export class AppComponent implements OnInit {
         fechaLanzamiento: new Date('2000-11-15'),
         precio: 499.99
       }]
-    }, 1000);
+    }, 3000);
   }
   title = 'front-end';
 
   peliculasEnCines;
-  peliculasProximosEstrenos = [{
-      titulo: 'avengers end game',
-      fechaLanzamiento: new Date(),
-      precio: 1499.99
-    },{
-      titulo: 'Inception',
-      fechaLanzamiento: new Date('2000-11-15'),
-      precio: 499.99
-  },{
-    titulo: 'Dragon Balll',
-    fechaLanzamiento: new Date('2000-11-15'),
-    precio: 399.99
-  }
-]
+  peliculasProximosEstrenos = [];
 
   duplicarNumero(valor: number): number {
     return valor * 2;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListadoPeliculasComponent } from './peliculas/listado-peliculas/listado-peliculas.component';
+import { ListadoGenericoComponent } from './utilidades/listado-generico/listado-generico.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListadoPeliculasComponent
+    ListadoPeliculasComponent,
+    ListadoGenericoComponent
   ],
   imports: [
     BrowserModule,
